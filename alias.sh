2192ffee2d2efcb26e28e4ef189303c7a9acb34a
b1n <120295547+b1nhack@users.noreply.github.com>
2024-03-23 01:09:43 +0800

    Added some bat alias

--- alias.sh
+++ alias.sh
@@ -4,3 +4,12 @@ alias hc="history -c"
 # bathelp
 alias bathelp='bat --plain --language=help'
 
+alias bgrep="batgrep"
+bman() {
+    BAT_THEME="Monokai Extended" batman "$@"
+    return $?
+}
+alias bpipe="batpipe"
+alias bwatch="batwatch"
+alias bdiff="batdiff"
+alias bpretty="prettybat"
