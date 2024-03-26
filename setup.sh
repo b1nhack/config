5ead82129723819c64ffa9834425205a8f7dabaa
b1n <120295547+b1nhack@users.noreply.github.com>
2024-03-27 02:40:17 +0800

    symbol link

---
+++ setup.sh
@@ -0,0 +1,2 @@
+ln -sf "./catppuccin/themes/Catppuccin Macchiato.tmTheme" "./bat/themes/Catppuccin Macchiato.tmTheme"
+ln -sf "./catppuccin/themes/catppuccin-macchiato.ini" "./zsh/plugins/fsh/themes/catppuccin-macchiato.ini"
